358c973aca9baac825b3ae5a73c8b6cadbe452c5
824792205 <824792205@qq.com>
2023-05-04 11:15:39 +0800

    add pod

--- TEST_TEXT/Pods/Target Support Files/Pods-TEST_TEXT/Pods-TEST_TEXT-frameworks.sh
+++ TEST_TEXT/Pods/Target Support Files/Pods-TEST_TEXT/Pods-TEST_TEXT-frameworks.sh
@@ -176,11 +176,13 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/MJRefresh/MJRefresh.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/OC_CWCarousel/OC_CWCarousel.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YYKit/YYKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/MJRefresh/MJRefresh.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/OC_CWCarousel/OC_CWCarousel.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YYKit/YYKit.framework"
